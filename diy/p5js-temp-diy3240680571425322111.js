da97bd5032814ff4d4584b8421fe48a1066c560f
pnwin <powhalenn@gmail.com>
2022-03-19 21:29:25 -0700

    Update p5js-temp-diy3240680571425322111.js

--- diy/p5js-temp-diy3240680571425322111.js
+++ diy/p5js-temp-diy3240680571425322111.js
@@ -3,24 +3,35 @@ var initials ='pn'; // your initials
 var choice = '1'; // starting choice, so it is not empty
 var screenbg = 250; // off white background
 var lastscreenshot=61; // last screenshot never taken
-
+var x = 0;
+var y = 0;
+var px = 0;
+var py = 0;
+var easing = 0.05;
 function preload() {
-// preload() runs once, it may make you wait
-//  img = loadImage('cat.jpg');  // cat.jpg needs to be next to this .js 2
-// you can link to an image on your github account
-  img = loadImage('https://pnwin.github.io/diy/images/brush.png');
-  img2 = loadImage('https://c.tenor.com/uj4Cnt7RVE0AAAAM/fatdog-dog.gif');
-  img3 = loadImage('https://pnwin.github.io/diy/images/litwick.png');
-  img4 = loadImage('https://pnwin.github.io/diy/images/house.png');
+  img = loadImage('https://pnwin.github.io/diy/images/bigpc.png');
+  img2 = loadImage('https://pnwin.github.io/diy/images/Mouse.png');
+  img3 = loadImage('https://pnwin.github.io/diy/images/Deskpad.png');
+  img4 = loadImage('https://pnwin.github.io/diy/images/Keyboard.png');
+  img5 = loadImage('https://pnwin.github.io/diy/images/Monitor.png');
+  img6 = loadImage('https://pnwin.github.io/diy/images/Monitorstand.png');
+  img7 = loadImage('https://pnwin.github.io/diy/images/Mouse.png');
+  img8 = loadImage('https://pnwin.github.io/diy/images/Desk.jpeg');
+  img9 =  loadImage('https://pnwin.github.io/diy/images/Smallpc.png');
+  img10 = loadImage('https://pnwin.github.io/diy/images/Ultra%20wide.png');
+  img11 = loadImage('https://pnwin.github.io/diy/images/Vertical%20monitor.png');
+  img12 =  loadImage('https://pnwin.github.io/diy/images/Wireless%20mouse.png')
+  img13 =  loadImage('https://pnwin.github.io/diy/images/Towerspeakers.png')
+  img14 =  loadImage('https://pnwin.github.io/diy/images/Bar.png')
+  img15 = loadImage('https://pnwin.github.io/diy/images/Plant.png')
 }
 
 function setup() {
 createCanvas(600, 600);  // canvas size
-background(150,220,2255);   // use our background screen color
-
+background(143,143,143)
 }
 
-var x = 0;
+var x = 1;
 var y = 0;
 var px = 0;
 var py = 0;
@@ -42,81 +53,131 @@ function newkeyChoice(toolChoice) { //toolchoice is the key that was pressed
   // graphic function
 
 
-  //Hotkey 1
-  if (toolChoice == '1' ) {  
-    stroke(255,255,255,100);
-   
-   strokeWeight(100)
-    line(mouseX, mouseY, pmouseX, pmouseY);
-    
-//Hotkey 2
-    noStroke()
-    fill(0,150,0)
- } else if (toolChoice == '2') {
-rect(mouseX,mouseY,400,200)
-
-   
-
-//Hotkey 3
- } else if (toolChoice == '3') {
-
-    stroke(0, 100, 0, 80);
-   strokeWeight(20)
-    line(mouseX, mouseY, pmouseX, pmouseY);
-  } else if (toolChoice == '4') {
-
-    stroke(0, 0, 255);
-    line(mouseX, mouseY, pmouseX, pmouseY);
-  } else if (key == '5') { // this tool calls a function
-    stroke(0, 0, 255);
-    testbox(20, 20, 200);
-    testbox(200, 20, 20);
-    
- // make testbox do something!
- //   line(mouseX, mouseY, pmouseX, pmouseY);
-  } else if (toolChoice == '6') {
-
-    stroke(200);
-    line(mouseX, mouseY, pmouseX, pmouseY);
- 
-//Hotkey 7 Toothbrush"
-  } else if (toolChoice == '7') {
- fill(255);
-    triangle(mouseX, mouseY, mouseX-20, mouseY-20,pmouseX+20,pmouseY-20);
+  //Core Peripherals
   
-  } else if (toolChoice == '8') {
+    //Desk+Reset Setup
+  if (toolChoice == '1' ) {  
+    image(img8, 0, 0,600,600);
 
-    fill(300, 100, 0, 80);
-    rect(mouseX, mouseY, 20, 20);
-  } else if (toolChoice == '9') {
-
-    fill(300, 100, 0, 80);
-    rect(mouseX, mouseY, 40, 40);
-  } else if (toolChoice == '0') {
-    stroke(0, 0);
-    fill(random(255), random(255), random(255), random(255));
-    rect(mouseX, mouseY, 200, 150);
-
-//img1 tool 
-  } else if (toolChoice == 'g' || toolChoice == 'G') { // g places the image we pre-loaded
-    blendMode(DARKEST)
-    image(img, mouseX-50, mouseY-50,50,50);
+ 
+    //Desk Pad
+    } else if (toolChoice == '2') {  
+    image(img3, mouseX-295, mouseY-290,600,600);  
+      
     
-//img2 tool
-     } else if (toolChoice == 'h' || toolChoice == 'H') { // h places the image we pre-loaded
-    blendMode(NORMAL)
-    image(img2, mouseX, mouseY,200,200);
+   //Keyboard
+ } else if (toolChoice == '3' ) {  
+    image(img4, mouseX-300, mouseY-300,600,600);
+    
 
-//img3 tool
-     } else if (toolChoice == 'j' || toolChoice == 'J') { // j places the image we pre-loaded
-       filter(BLUR,3)
-    image(img3, mouseX, mouseY,86,144);
+    //Mouse
+  } else if (toolChoice == '4') {  
+    image(img2, mouseX-298, mouseY-290,600,600);}   
+
+    
+    //Wireless Mouse
+  if (toolChoice == '5' ) {  
+    image(img12, mouseX-298, mouseY-290,600,600); }  
+ 
+    
+    //Monitor
+  if (toolChoice == '6' ) {  
+    image(img5, mouseX-300, mouseY-300,600,600); }  
+
+    
+    //Vertical Monitor
+  if (toolChoice == '7' ) {  
+    image(img11, mouseX-300, mouseY-300,600,600); }  
+
+    
+    //Ultrawide Monitor
+  if (toolChoice == '8' ) {  
+    image(img10, mouseX-300, mouseY-270,600,600) }  
+ 
+    
+    //Monitor Stand
+  if (toolChoice == '9' ) {  
+    image(img6, mouseX-295, mouseY-330,600,600);  } 
+  
+    
+    //Big PC
+  if (toolChoice == '0' ) {  
+    image(img, mouseX-300, mouseY-380,600,600); }  
+  
+    
+    //Small PC
+  if (toolChoice == '-' ) {  
+    image(img9, mouseX-300, mouseY-340,600,600); } 
+  
+  //Stereo Speakers
+  if (toolChoice == 'o' ) {  
+    image(img13, mouseX-295, mouseY-290,600,600); } 
+  
+  //Bar Speakers
+  if (toolChoice == 'i' ) {  
+    image(img14, mouseX-295, mouseY-300,600,600); } 
+  
+   //Plant
+  if (toolChoice == 'u' ) {  
+    image(img15, mouseX-295, mouseY-300,600,600); } 
+  
+  
+  //Brushes
+ 
+   //Eraser
+  noStroke()
+  if (toolChoice == 'e' ) { 
+    fill(143,143,143)
+    ellipse(mouseX,mouseY,20,20)} 
+  
+  //LED Light Strip
+  if (toolChoice == 't' ) { 
+   noFill()
+    stroke(220,220,255)
+    strokeWeight(15)
+    drawingContext.shadowBlur=32;
+    drawingContext.shadowColor = color(150,220,255);
+   line(mouseX,mouseY,mouseX,pmouseY)
+    blendMode(BLEND)
+  }
+  
+//Scene Coloring
+
+  //Blue
+   if (toolChoice == 'j' ) { 
+  fill(150,220,255,50)
+  rect(0,0,1000,1000)
+      blendMode(BURN) 
+}
+
+  //White
+   if (toolChoice == 'k' ) { 
+  fill(255,255,255,20)
+  rect(0,0,1000,1000)
+      blendMode(DODGE) 
+}
+     
+     
+  //Purple
+   if (toolChoice == 'l' ) { 
+  fill(180,50,255,20)
+  rect(0,0,1000,1000)
+      blendMode(BURN) 
+}
+     
+   //Red
+   if (toolChoice == 'b' ) { 
+  fill(255,0,0,30)
+  rect(0,0,1000,1000)
+   blendMode(BURN)}
+  
        
-//img4 tool
-     } else if (toolChoice == 'k' || toolChoice == 'K') { // k places the image we pre-loaded
-       filter(BLUR,0)
-    image(img4, mouseX, mouseY,400,400);
-     } 
+   //Green
+   if (toolChoice == 'b' ) { 
+  fill(10,255,100,70)
+  rect(0,0,1000,1000)
+   blendMode(MULTIPLY) 
+  }
 }
 
 function testbox(r, g, b) {
